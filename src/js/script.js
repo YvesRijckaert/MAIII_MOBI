36bdc979ee948e0772d309b22b75f7720b7b7eaf
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-01-25 09:12:10 +0100

    Added more progressive enhancement code to javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,34 @@
 const form = document.querySelector(`.filter-tag`);
+const eventSection = document.querySelector(`.event-section`);
+let searchTerm;
+let results;
 
 const init = () => {
+  //Progressive enhancement: AJAX tags: IF javascript works, this code will be executed:
   if (location.search === `?page=events`) { //if we're on the events page
-    form.addEventListener(`change`, submitHandler);
+    document.querySelector(`.submit-button`).classList.add(`hide`); //hide the submit button
+    eventSection.innerHTML = ``;
+    form.addEventListener(`change`, submitHandler); //listen to the change event
   }
 };
 
 const parseData = data => {
-  console.log(data);
+  results = data;
+  console.log(results);
+  console.log(searchTerm);
 };
 
 const submitHandler = e => {
+  searchTerm = e.target.value;
   e.preventDefault();
-
-  fetch(window.location.href, { //laad de huidige pagina
+  fetch(window.location.href, {
     headers: new Headers({
-      Accept: `application/json` //accepteer JSON
+      Accept: `application/json`
     }),
-    credentials: `same-origin`, //zelfde pagina
-    method: `post`, //POST methode
+    credentials: `same-origin`,
+    method: `post`,
   })
-    .then(r => r.json()) //de response is JSON
+    .then(r => r.json())
     .then(jsonObj => {
       parseData(jsonObj);
     });
