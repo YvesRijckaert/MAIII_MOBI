ae6623e2195347b8eddd439bd0d61603628d8562
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-01-27 17:30:01 +0100

    Added more CSS styling to the homepage, need to change from GRID to FLEXBOX in certain situations

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -7,10 +7,10 @@
       <header class="about-header">
         <h2 class="section-title">Week van de mobiliteit?</h2>
       </header>
-      <img class="about-image" height="434" width="578" src="assets/img/vrouwen-markt-fiets.jpg" alt="Vrouwen op de markt met hun fiets">
+      <img class="about-image" height="434" width="578" src="assets/img/vrouwen-markt-fiets.jpg" alt="Vrouwen op de markt met hun fiets" />
       <p class="about-text">Elk jaar zet de Week van de Mobiliteit (16 – 22 september) Vlaanderen #goedopweg om mee(r) te autominderen. Tal van acties tijdens de Week doen ons stil staan bij ons verplaatsingsgedrag en laten proeven van de alternatieven. Want wie de overstap maakt ontdekt zelf de voordelen van het stappen, trappen, trein/tram/bus of autodelen.</p>
-      <a class="about-button-1" href="#">Vorige edities</a>
-      <a class="about-button-2" href="#">&#35;goedopweg</a>
+      <a class="button" href="#">vorige edities</a>
+      <a class="button" href="#">&#35;goedopweg</a>
     </div>
   </section>
 
@@ -19,12 +19,17 @@
       <header>
         <h2 class="section-title">In de kijker</h2>
       </header>
-      <img class="image" height="434" width="578" src="assets/img/man-fiets.jpg" alt="Man fietst aan het MAS in Antwerpen">
-      <ul>
-        <li>Expo Mobility</li>
-        <li>Le beau V</li>
-        <li>Bike Brussels</li>
-      </ul>
+      <article class="uitgelicht-content">
+        <picture class="uitgelicht-image">
+          <img height="434" width="578" src="assets/img/man-fiets.jpg" alt="Man fietst aan het MAS in Antwerpen" />
+        </picture>
+        <div class="uitgelicht-events">
+          <a data-date="Ma 16" class="uitgelicht-event" href="#">Expo Mobility</a>
+          <a data-date="Wo 18" class="uitgelicht-event" href="#">Le beau V</a>
+          <a data-date="Do 19" class="uitgelicht-event" href="#">Bike Brussels</a>
+          <a class="button" href="index.php?page=events">alle activiteiten</a>
+        </div>
+      </article>
     </div>
   </section>
 
@@ -33,8 +38,12 @@
       <header>
         <h2 class="section-title">Organiseer mee</h2>
       </header>
-      <img class="image" height="434" width="578" src="assets/img/meisje-schmink.jpg" alt="Meisje wordt geschminkt">
-      <p>Registreer jouw mobiliteitsactie. Zo kunnen we je initiatief mee promoten en brengen we de dynamiek voor duurzame mobiliteit tijdens de Week hier in beeld. Want samen zijn we #goedopweg! Trouwens, we hebben ook leuk kant-en-klaar promotiemateriaal voor je klaar!</p>
+      <article class="organiseer-content">
+        <picture class="organiseer-image">
+          <img class="image" height="434" width="578" src="assets/img/meisje-schmink.jpg" alt="Meisje wordt geschminkt" />
+        </picture>
+        <p class="organiseer-text">Registreer jouw mobiliteitsactie. Zo kunnen we je initiatief mee promoten en brengen we de dynamiek voor duurzame mobiliteit tijdens de Week hier in beeld. Want samen zijn we #goedopweg! Trouwens, we hebben ook leuk kant-en-klaar promotiemateriaal voor je klaar!</p>
+      </article>
     </div>
   </section>
 
@@ -44,7 +53,7 @@
         <h2 class="section-title">Wistjedatjes</h2>
       </header>
       <ul>
-        <img class="image" height="376" width="452" src="assets/img/auto.svg" alt="Een auto">
+        <img class="image" height="376" width="452" src="assets/img/auto.svg" alt="Een auto" />
         <li><a href="#">637 verkeersdoden per jaar</a></li>
         <li><a href="#">doodsoorzaak nr. 1 bij -35 jarigen</a></li>
         <li><a href="#">trein 75&#37; minder CO<sub>2</sub> dan auto</a></li>
@@ -81,9 +90,9 @@
       <header>
         <h2 class="section-title">Instagram &#35;goedopweg</h2>
       </header>
-      <img class="image" height="380" width="380" src="assets/img/zeepkistenrace.jpg" alt="Zeepkistenrace">
-      <img class="image" height="380" width="380" src="assets/img/ganzen.jpg" alt="Meisjes voederen ganzen">
-      <img class="image" height="380" width="380" src="assets/img/bakfiets.jpg" alt="Man met bakfiets">
+      <img class="image" height="380" width="380" src="assets/img/zeepkistenrace.jpg" alt="Zeepkistenrace" />
+      <img class="image" height="380" width="380" src="assets/img/ganzen.jpg" alt="Meisjes voederen ganzen" />
+      <img class="image" height="380" width="380" src="assets/img/bakfiets.jpg" alt="Man met bakfiets" />
     </div>
   </section>
 </main>
