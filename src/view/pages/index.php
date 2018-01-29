c6f638f577965dcd705108303386e28e1834f779
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-01-29 02:09:24 +0100

    Added webp support for the images, some responsive styling added, picture element added

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -2,13 +2,37 @@
   <?php if(!empty($_GET['activity_id'])){
     echo $test;
   } ?>
+  <!-- <section class="above-the-fold">
+    <div class="wrap">
+      <h2 class="above-the-fold-title">Week<br/>van de<br/> mobiliteit</h2>
+      <p>16 tot 22 september 2018</p>
+      <a class="button" href="index.php?page=events">alle activiteiten</a>
+    </div>
+  </section> -->
   <section class="about">
     <div class="wrap">
       <header class="about-header">
         <h2 class="section-title">Week van de mobiliteit?</h2>
       </header>
       <div class="about-content">
-        <img class="about-image" height="434" width="578" src="assets/img/vrouwen-markt-fiets.jpg" alt="Vrouwen op de markt met hun fiets" />
+        <picture class="about-image">
+          <source media="(min-width: 1440px)" srcset="assets/img/vrouwen-markt-fiets/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/vrouwen-markt-fiets/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/vrouwen-markt-fiets/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/vrouwen-markt-fiets/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/vrouwen-markt-fiets/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/vrouwen-markt-fiets/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/vrouwen-markt-fiets/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/vrouwen-markt-fiets/mini.jpg" />
+
+          <img class="about-image"
+          height="434" width="578"
+          srcset="assets/img/vrouwen-markt-fiets/full.jpg 1156w,
+                  assets/img/vrouwen-markt-fiets/medium.jpg 933w,
+                  assets/img/vrouwen-markt-fiets/small.jpg 566w,
+                  assets/img/vrouwen-markt-fiets/mini.jpg 411w"
+          alt="Vrouwen op de markt met hun fiets" />
+        </picture>
         <p class="about-text">Elk jaar zet de Week van de Mobiliteit (16 – 22 september) Vlaanderen #goedopweg om mee(r) te autominderen. Tal van acties tijdens de Week doen ons stil staan bij ons verplaatsingsgedrag en laten proeven van de alternatieven. Want wie de overstap maakt ontdekt zelf de voordelen van het stappen, trappen, trein/tram/bus of autodelen.</p>
       </div>
       <div class="about-buttons">
@@ -25,7 +49,21 @@
       </header>
       <div class="uitgelicht-content">
         <picture class="uitgelicht-image">
-          <img height="434" width="578" src="assets/img/man-fiets.jpg" alt="Man fietst aan het MAS in Antwerpen" />
+          <source media="(min-width: 1440px)" srcset="assets/img/man-fiets/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/man-fiets/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/man-fiets/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/man-fiets/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/man-fiets/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/man-fiets/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/man-fiets/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/man-fiets/mini.jpg" />
+          <img class="uitgelicht-image"
+          height="434" width="578"
+          srcset="assets/img/man-fiets/full.jpg 1156w,
+                  assets/img/man-fiets/medium.jpg 933w,
+                  assets/img/man-fiets/small.jpg 566w,
+                  assets/img/man-fiets/mini.jpg 411w"
+          alt="Man fietst aan het MAS in Antwerpen" />
         </picture>
         <div class="uitgelicht-events">
           <a data-date="Ma 16" class="uitgelicht-event" href="#">Expo Mobility</a>
@@ -44,7 +82,21 @@
       </header>
       <div class="organiseer-content">
         <picture class="organiseer-image">
-          <img class="image" height="434" width="578" src="assets/img/meisje-schmink.jpg" alt="Meisje wordt geschminkt" />
+          <source media="(min-width: 1440px)" srcset="assets/img/meisje-schmink/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/meisje-schmink/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/meisje-schmink/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/meisje-schmink/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/meisje-schmink/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/meisje-schmink/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/meisje-schmink/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/meisje-schmink/mini.jpg" />
+          <img class="organiseer-image"
+          height="434" width="578"
+          srcset="assets/img/meisje-schmink/full.jpg 1156w,
+                  assets/img/meisje-schmink/medium.jpg 933w,
+                  assets/img/meisje-schmink/small.jpg 566w,
+                  assets/img/meisje-schmink/mini.jpg 411w"
+          alt="Meisje wordt geschminkt" />
         </picture>
         <div class="organiseer-bodycopy">
           <p class="organiseer-text">Registreer jouw mobiliteitsactie. Zo kunnen we je initiatief mee promoten en brengen we de dynamiek voor duurzame mobiliteit tijdens de Week hier in beeld. Want samen zijn we #goedopweg! Trouwens, we hebben ook leuk kant-en-klaar promotiemateriaal voor je klaar!</p>
@@ -106,9 +158,57 @@
         <h2 class="section-title">Instagram &#35;goedopweg</h2>
       </header>
       <div class="instagram-content">
-        <img class="image" height="380" width="380" src="assets/img/zeepkistenrace.jpg" alt="Zeepkistenrace" />
-        <img class="image" height="380" width="380" src="assets/img/ganzen.jpg" alt="Meisjes voederen ganzen" />
-        <img class="image" height="380" width="380" src="assets/img/bakfiets.jpg" alt="Man met bakfiets" />
+        <picture class="instagram-image">
+          <source media="(min-width: 1440px)" srcset="assets/img/zeepkistenrace/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/zeepkistenrace/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/zeepkistenrace/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/zeepkistenrace/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/zeepkistenrace/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/zeepkistenrace/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/zeepkistenrace/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/zeepkistenrace/mini.jpg" />
+          <img class="instagram-image"
+          height="380" width="380"
+          srcset="assets/img/zeepkistenrace/full.jpg 760w,
+                  assets/img/zeepkistenrace/medium.jpg 580w,
+                  assets/img/zeepkistenrace/small.jpg 480w,
+                  assets/img/zeepkistenrace/mini.jpg 411w"
+          alt="Zeepkistenrace" />
+        </picture>
+        <picture class="instagram-image">
+          <source media="(min-width: 1440px)" srcset="assets/img/ganzen/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/ganzen/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/ganzen/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/ganzen/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/ganzen/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/ganzen/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/ganzen/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/ganzen/mini.jpg" />
+          <img class="instagram-image"
+          height="380" width="380"
+          srcset="assets/img/ganzen/full.jpg 760w,
+                  assets/img/ganzen/medium.jpg 580w,
+                  assets/img/ganzen/small.jpg 480w,
+                  assets/img/ganzen/mini.jpg 411w"
+          alt="Zeepkistenrace" />
+        </picture>
+        <picture class="instagram-image">
+          <source media="(min-width: 1440px)" srcset="assets/img/bakfiets/full.webp" type="image/webp"/>
+          <source media="(min-width: 1440px)" srcset="assets/img/bakfiets/full.jpg" />
+          <source media="(min-width: 1024px)" srcset="assets/img/bakfiets/medium.webp"  type="image/webp" />
+          <source media="(min-width: 1024px)" srcset="assets/img/bakfiets/medium.jpg" />
+          <source media="(min-width: 768px)" srcset="assets/img/bakfiets/small.webp"  type="image/webp" />
+          <source media="(min-width: 768px)" srcset="assets/img/bakfiets/small.jpg" />
+          <source media="(min-width: 320px)" srcset="assets/img/bakfiets/mini.webp"  type="image/webp" />
+          <source media="(min-width: 320px)" srcset="assets/img/bakfiets/mini.jpg" />
+          <img class="instagram-image"
+          height="380" width="380"
+          srcset="assets/img/bakfiets/full.jpg 760w,
+                  assets/img/bakfiets/medium.jpg 580w,
+                  assets/img/bakfiets/small.jpg 480w,
+                  assets/img/bakfiets/mini.jpg 411w"
+          alt="Man op bakfiets in Kortrijk" />
+        </picture>
         <a target="_blank" class="button" href="https://www.instagram.com/weekvandemobiliteit/">Volg ons op instagram</a>
       </div>
     </div>
